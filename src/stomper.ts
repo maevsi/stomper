12b9e1bb9a82ade7d2e5105f4cbb208dd9cb058a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-04-10 10:22:31 +0200

    eslint: Add

--- src/stomper.ts
+++ src/stomper.ts
@@ -1,47 +1,47 @@
-import fs = require('fs');
+import { Client } from '@stomp/stompjs'
 
-import { Client } from '@stomp/stompjs';
+import fs = require('fs')
 
-Object.assign(global, { WebSocket: require('websocket').w3cwebsocket });
+Object.assign(global, { WebSocket: require('websocket').w3cwebsocket })
 
 if (
-        process.env.RABBITMQ_USER_FILE === undefined
-    ||  process.env.RABBITMQ_PASS_FILE === undefined
+  process.env.RABBITMQ_USER_FILE === undefined ||
+  process.env.RABBITMQ_PASS_FILE === undefined
 ) {
-    console.error("Missing environment variables!")
-    process.exit(1)
+  console.error('Missing environment variables!')
+  process.exit(1)
 }
 
 const client = new Client({
-    brokerURL: "ws://rabbitmq:15674/ws",
-    connectHeaders: {
-        login: fs.readFileSync(process.env.RABBITMQ_USER_FILE, 'utf8'),
-        passcode: fs.readFileSync(process.env.RABBITMQ_PASS_FILE, 'utf8')
-    },
-    debug: function (str) {
-        if (process.env.NODE_ENV !== 'production') {
-            console.log(str);
-        }
+  brokerURL: 'ws://rabbitmq:15674/ws',
+  connectHeaders: {
+    login: fs.readFileSync(process.env.RABBITMQ_USER_FILE, 'utf8'),
+    passcode: fs.readFileSync(process.env.RABBITMQ_PASS_FILE, 'utf8')
+  },
+  debug: function (str) {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(str)
     }
-});
+  }
+})
 
 client.onConnect = function (frame) {
-    client.subscribe("/queue/message_queue", function (message) {
-        // JSON.parse(message.body)
-        if (message.body) {
-            console.log("got message with body " + message.body)
-        } else {
-            console.log("got empty message");
-        }
+  client.subscribe('/queue/message_queue', function (message) {
+    // JSON.parse(message.body)
+    if (message.body) {
+      console.log('got message with body ' + message.body)
+    } else {
+      console.log('got empty message')
+    }
 
-        message.ack();
-    }, { ack: 'client' });
-};
+    message.ack()
+  }, { ack: 'client' })
+}
 
 client.onStompError = function (frame) {
-    console.log('Broker reported error: ' + frame.headers['message']);
-    console.log('Additional details: ' + frame.body);
-    process.exit(1)
-};
+  console.log('Broker reported error: ' + frame.headers.message)
+  console.log('Additional details: ' + frame.body)
+  process.exit(1)
+}
 
-client.activate();
+client.activate()
