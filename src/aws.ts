d899107cb334640de207115b052b5715fbbb1a18
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-04-13 01:27:20 +0200

    aws: Switch To SES v2 Raw Emails
    So that iCal data can be attached.

--- src/aws.ts
+++ src/aws.ts
@@ -1,52 +1,41 @@
-import { config, SES } from 'aws-sdk'
+import { config, SESV2 } from 'aws-sdk'
+import MailComposer from 'nodemailer/lib/mail-composer'
 
 config.apiVersions = {
-  ses: '2010-12-01'
+  ses: '2019-09-27'
 }
-
 config.loadFromPath('/run/secrets/stomper_aws')
 
-var ses = new SES()
+var ses = new SESV2()
 var params = {
-  Destination: { /* required */
-    //   BccAddresses: [
-    //     'STRING_VALUE',
-    //     /* more items */
-    //   ],
-    //   CcAddresses: [
-    //     'STRING_VALUE',
-    //     /* more items */
-    //   ],
+  Destination: {
     ToAddresses: [
       'e-mail@jonas-thelemann.de'
     ]
-  },
-  Source: 'Maevsi <noreply@maev.si>', /* required */
-  Template: 'MyTemplate', /* required */
-  TemplateData: '{ "name":"Jonas", "favoriteanimal": "dog" }', /* required */
-  ConfigurationSetName: 'default'
-  // ReplyToAddresses: [
-  //   'STRING_VALUE',
-  //   /* more items */
-  // ],
-  // ReturnPath: 'STRING_VALUE',
-  // ReturnPathArn: 'STRING_VALUE',
-  // SourceArn: 'STRING_VALUE',
-  // Tags: [
-  //   {
-  //     Name: 'STRING_VALUE', /* required */
-  //     Value: 'STRING_VALUE' /* required */
-  //   },
-  //   /* more items */
-  // ],
-  // TemplateArn: 'STRING_VALUE'
+  }
 }
 
 export function trigger (templateData: string) {
-  params.TemplateData = templateData
-
-  ses.sendTemplatedEmail(params, function (err, data) {
-    if (err) console.log(err, err.stack) // an error occurred
-    else console.log(data) // successful response
+  new MailComposer({
+    from: '"Maevsi" <noreply@maev.si>',
+    html: '<h1>h1</h1>',
+    subject: 'SES',
+    text: 'Hello'
   })
+    .compile().build(function (err, message) {
+      console.error(err)
+
+      var paramsContent = {
+        Content: {
+          Raw: {
+            Data: message
+          }
+        }
+      }
+
+      ses.sendEmail({ ...params, ...paramsContent }, function (err, data) {
+        if (err) console.error(err, err.stack)
+        else console.log(data)
+      })
+    })
 }
