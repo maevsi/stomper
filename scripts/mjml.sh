2e6f8f3b5986c606d0fc1d8da65987ad02983dc0
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-12-27 07:46:32 +0100

    fix: use dist directory for build's email templates

---
+++ scripts/mjml.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Get this script's directory
+THIS=$(dirname "$(readlink -f "$0")")
+
+mkdir -p "$THIS/../dist/email-templates/"
+find "$THIS/../src/email-templates/" -maxdepth 1 -type f -name '*.mjml' -exec mjml -o "$THIS/../dist/email-templates/" -r {} \;
