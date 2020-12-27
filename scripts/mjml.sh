6b9b6994bd4cc48aa63df50377bc6cca5d196cc6
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-12-27 08:06:10 +0100

    fix(webserver): copy assets to dist

--- scripts/mjml.sh
+++ scripts/mjml.sh
@@ -4,4 +4,6 @@
 THIS=$(dirname "$(readlink -f "$0")")
 
 mkdir -p "$THIS/../dist/email-templates/"
+
+cp "$THIS/../assets/" "$THIS/../dist/" -R
 find "$THIS/../src/email-templates/" -maxdepth 1 -type f -name '*.mjml' -exec mjml -o "$THIS/../dist/email-templates/" -r {} \;
