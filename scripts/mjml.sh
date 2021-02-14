0a5ec87e0645cd8074abdbc8e80524deaa9ae4d0
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-02-14 06:12:22 +0100

    fix(scripts): add mjml path

--- scripts/mjml.sh
+++ scripts/mjml.sh
@@ -6,4 +6,4 @@ THIS=$(dirname "$(readlink -f "$0")")
 mkdir -p "$THIS/../dist/email-templates/"
 
 cp "$THIS/../assets/" "$THIS/../dist/" -R
-find "$THIS/../src/email-templates/" -maxdepth 1 -type f -name '*.mjml' -exec mjml -o "$THIS/../dist/email-templates/" -r {} \;
+find "$THIS/../src/email-templates/" -maxdepth 1 -type f -name '*.mjml' -exec "$THIS/../node_modules/.bin/mjml" -o "$THIS/../dist/email-templates/" -r {} \;
