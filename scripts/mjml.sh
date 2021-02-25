bf02c38fe56cc685ff8397d193a01d7ce6a9075d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-02-25 22:38:08 +0100

    fix(assets): improve accessibility for development

--- scripts/mjml.sh
+++ scripts/mjml.sh
@@ -5,5 +5,5 @@ THIS=$(dirname "$(readlink -f "$0")")
 
 mkdir -p "$THIS/../dist/email-templates/"
 
-cp "$THIS/../assets/" "$THIS/../dist/" -R
+cp "$THIS/../src/assets/" "$THIS/../dist/" -R
 find "$THIS/../src/email-templates/" -maxdepth 1 -type f -name '*.mjml' -exec "$THIS/../node_modules/.bin/mjml" -o "$THIS/../dist/email-templates/" -r {} \;
