534752156e972b280391bd435f8be4f9aa5d3ee9
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-02-25 08:47:46 +0100

    feat: add i18n

---
+++ src/handlebars.ts
@@ -0,0 +1,49 @@
+import fs from 'fs'
+import Handlebars from 'handlebars'
+import i18next from 'i18next'
+import Backend from 'i18next-fs-backend'
+import path from 'path'
+import { Template } from './types'
+
+const HandlebarsI18n = require('./handlebars-i18n.js')
+
+const STACK_DOMAIN = process.env.STACK_DOMAIN || 'maevsi.test'
+
+i18next
+  .use(Backend)
+  .init({
+    backend: {
+      addPath: path.join(__dirname, './locales/{{lng}}/{{ns}}.missing.json'),
+      loadPath: path.join(__dirname, './locales/{{lng}}/{{ns}}.json')
+    },
+    debug: process.env.NODE_ENV !== 'production',
+    defaultNS: 'maevsi',
+    fallbackLng: process.env.NODE_ENV !== 'production' ? 'dev' : 'en',
+    initImmediate: false,
+    lng: 'en',
+    ns: ['accountRegistration', 'accountPasswordResetRequest', 'maevsi'],
+    preload: fs.readdirSync(path.join(__dirname, './locales')).filter((fileName: any) => {
+      const joinedPath = path.join(path.join(__dirname, './locales'), fileName)
+      const isDirectory = fs.lstatSync(joinedPath).isDirectory()
+      return isDirectory
+    })
+  })
+
+HandlebarsI18n.init()
+
+export function renderTemplate (args: Template) {
+  if (process.env.NODE_ENV !== 'production') {
+    i18next.reloadResources()
+  }
+
+  const template = Handlebars.compile(fs.readFileSync(path.resolve(__dirname, `./email-templates/${args.templateNamespace}.html`), 'utf-8'))
+  i18next.changeLanguage(args.language)
+  return template({
+    stackDomain: STACK_DOMAIN,
+    ...args.templateVariables
+  })
+}
+
+export function i18nextResolve (id: string) {
+  return i18next.t(id)
+}
--- src/smtp.ts
+++ src/smtp.ts
@@ -1,13 +1,12 @@
 import fs from 'fs'
 import http from 'http'
-import path from 'path'
 
-import Handlebars from 'handlebars'
 import { htmlToText as htmlToTextImported } from 'html-to-text'
 import nodemailer from 'nodemailer'
 
 import { getContact, getEvent, getInvitation } from './database'
-import { MaevsiContact, MaevsiEvent, MaevsiInvitation, MessageInvitation, SendMailConfig } from './types'
+import { MaevsiContact, MaevsiEvent, MaevsiInvitation, MailTemplate, MailWithContent, MessageInvitation } from './types'
+import { i18nextResolve, renderTemplate } from './handlebars'
 
 const camelcaseKeys = require('camelcase-keys')
 const consola = require('consola')
@@ -15,7 +14,6 @@ const consola = require('consola')
 const HTML_TO_TEXT_OPTIONS = { tags: { img: { format: 'skip' } } }
 const MAIL_FROM = '"maevsi" <noreply@maev.si>'
 const SECRET_STOMPER_NODEMAILER_TRANSPORTER_PATH = '/run/secrets/stomper_nodemailer-transporter'
-const STACK_DOMAIN = process.env.STACK_DOMAIN || 'maevsi.test'
 
 if (!fs.existsSync(SECRET_STOMPER_NODEMAILER_TRANSPORTER_PATH)) {
   consola.error('The STMP configuration secret is missing!')
@@ -28,47 +26,44 @@ function htmlToText (html: string) {
   return htmlToTextImported(html, HTML_TO_TEXT_OPTIONS)
 }
 
-async function sendMail (sendMailConfig: SendMailConfig) {
+async function sendMail (mailWithConfig: MailWithContent) {
   const mailSentData = await NODEMAILER_TRANSPORTER.sendMail({
     from: MAIL_FROM,
-    ...sendMailConfig
+    ...mailWithConfig
   })
 
   consola.log('Message sent: %s', mailSentData.messageId)
 }
 
-function sendMailTemplated (to: string, subject: string, templateFileName: string, templateVariables: object) {
-  const template = Handlebars.compile(fs.readFileSync(path.resolve(__dirname, `./email-templates/${templateFileName}.html`), 'utf-8'))
-  const html = template({
-    stackDomain: STACK_DOMAIN,
-    ...templateVariables
-  })
-
-  sendMail({ to, subject, html, text: htmlToText(html) })
+function sendMailTemplated (args: MailTemplate) {
+  const html = renderTemplate({ language: args.language, templateNamespace: args.templateNamespace, templateVariables: args.templateVariables })
+  sendMail({ to: args.to, subject: i18nextResolve(`${args.templateNamespace}:subject`), html, text: htmlToText(html) })
 }
 
 export function sendAccountPasswordResetRequestMail (dataJsonObject: any) {
-  sendMailTemplated(
-    dataJsonObject.account.email_address,
-    'Password Reset Request',
-    'accountPasswordResetRequest',
-    {
+  sendMailTemplated({
+    to: dataJsonObject.account.email_address,
+    subject: 'Password Reset Request',
+    language: dataJsonObject.template.language,
+    templateNamespace: 'accountPasswordResetRequest',
+    templateVariables: {
       passwordResetVerificationLink: `https://${process.env.STACK_DOMAIN || 'maevsi.test'}/task/account/password/reset?code=${dataJsonObject.account.password_reset_verification}`,
       username: dataJsonObject.account.username
     }
-  )
+  })
 }
 
 export function sendAccountRegisterMail (dataJsonObject: any) {
-  sendMailTemplated(
-    dataJsonObject.account.email_address,
-    'Welcome',
-    'accountRegister',
-    {
+  sendMailTemplated({
+    to: dataJsonObject.account.email_address,
+    subject: 'Welcome',
+    language: dataJsonObject.template.language,
+    templateNamespace: 'accountRegistration',
+    templateVariables: {
       emailAddressVerificationLink: `https://${process.env.STACK_DOMAIN || 'maevsi.test'}/task/account/email-address/verify?code=${dataJsonObject.account.email_address_verification}`,
       username: dataJsonObject.account.username
     }
-  )
+  })
 }
 
 export async function sendInvitationMail (dataJsonObject: MessageInvitation) {
