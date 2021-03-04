e8214ae2ca020375d73b39fedd7a59061c00636c
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-03-04 08:01:09 +0100

    fix(build): copy locales to dist

--- scripts/mjml.sh
+++ scripts/mjml.sh
@@ -6,4 +6,5 @@ THIS=$(dirname "$(readlink -f "$0")")
 mkdir -p "$THIS/../dist/email-templates/"
 
 cp "$THIS/../src/assets/" "$THIS/../dist/" -R
+cp "$THIS/../src/locales/" "$THIS/../dist/" -R
 find "$THIS/../src/email-templates/" -maxdepth 1 -type f -name '*.mjml' -exec "$THIS/../node_modules/.bin/mjml" -o "$THIS/../dist/email-templates/" -r {} \;
