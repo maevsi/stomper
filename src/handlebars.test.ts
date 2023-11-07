40550e6dcecdf77a6330ad74e1e4eb2964869957
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-11-07 23:32:10 +0100

    feat(locales): add hearts to description

--- src/handlebars.test.ts
+++ src/handlebars.test.ts
@@ -2,24 +2,24 @@ import { i18nextResolve, templateCompile } from './handlebars.js'
 
 test('compiles template', () => {
   expect(templateCompile('{{__ "maevsi:subtitle"}}', 'en', {})).toBe(
-    'Find events, guests and friends.',
+    'Find events, guests and friends 💙❤️💚',
   )
 })
 
 test('compiles template in another language', () => {
   expect(templateCompile('{{__ "maevsi:subtitle"}}', 'de', {})).toBe(
-    'Finde Veranstaltungen, Gäste und Freunde.',
+    'Finde Veranstaltungen, Gäste und Freunde 💙❤️💚',
   )
 })
 
 test('resolves template', () => {
   expect(i18nextResolve('maevsi:subtitle')).toBe(
-    'Find events, guests and friends.',
+    'Find events, guests and friends 💙❤️💚',
   )
 })
 
 test('resolves template in another language', () => {
   expect(i18nextResolve('maevsi:subtitle', 'de')).toBe(
-    'Finde Veranstaltungen, Gäste und Freunde.',
+    'Finde Veranstaltungen, Gäste und Freunde 💙❤️💚',
   )
 })
